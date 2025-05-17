80d9add24f95ce01649fe71db525132120538275
haroldbaes17 <hbarrantese@ucenfotec.ac.cr>
2025-05-17 11:34:05 -0600

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Instancia del gato
+        var myCat = new Cat() { Id = 1, Name = "Misingo" }; 
+
+        Console.WriteLine("Id del gato: " +  myCat.Id);
+        Console.WriteLine("Nombre del gato: " + myCat.Name);
+
+        Console.WriteLine("Ingresa una descripcion para el gato");
+
+        myCat.Description = Console.ReadLine();
+
+        Console.WriteLine("Descripcion: " + myCat.Description);
+    }
+}
